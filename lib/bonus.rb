3814cc4935fe928f3f7d7321cbea023b1aed2a66
Georgette Class-Peters <georgycp@udel.edu>
2020-08-04 18:18:04 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -3,10 +3,10 @@ def bonus
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo”, age: "15", status: "alive”},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-         {name: “Benvolio”, age: "17”, attitude: "worried"},
-         {name: “Steven”, age: "30", attitude: "confused”}
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Steven", age: "30", attitude: "confused"}
       ]
    },
    :capulet => {
